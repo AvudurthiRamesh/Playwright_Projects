af679c3acdb599ea87d7a3764554bac69e7672df
Ramesh Avudurthi <ramesh.avudurthi007@gmail.com>
2025-07-03 22:06:22 +0530

    Initial commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Login to the-internet.herokuapp.com', async ({ page }) => {
+  await page.goto('/login');
+  await page.fill('#username', 'tomsmith');
+  await page.fill('#password', 'SuperSecretPassword!');
+  await page.click('button[type="submit"]');
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+});
